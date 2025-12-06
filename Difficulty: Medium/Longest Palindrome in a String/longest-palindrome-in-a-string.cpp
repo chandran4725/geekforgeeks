493744ed2262e43946e59e838035f5eaa3c6b1dc
chandran4725 <chandran.g.work@gmail.com>
2025-12-06 13:42:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
@@ -3,14 +3,14 @@ class Solution {
     
     string longestPalindrome(string &S) {
         int start = 0, end = 0;
-    int low,high;
-    for(int i = 0; i < S.length(); i++){
+        int low,high;
+        for(int i = 0; i < S.length(); i++){
         // odd part
-         low = i - 1;
-         high = i;
-        while(low>=0 && high<S.length() && S[low] == S[high]){
+             low = i - 1;
+            high = i;
+            while(low>=0 && high<S.length() && S[low] == S[high]){
             
-            if(high - low + 1 > end){
+                if(high - low + 1 > end){
                 end = high - low + 1; //current longest pallindrome length
                 start = low;
             }
@@ -30,8 +30,8 @@ class Solution {
             high++;
         }
         
-    }
-    if(end == 0)return S.substr(start,1); // no pallindrome at all
-    return S.substr(start,end);
+        }
+        if(end == 0)return S.substr(start,1); // no pallindrome at all
+        return S.substr(start,end);
     }
 };
