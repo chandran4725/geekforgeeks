e994670259212f851dcc8af4f3b07f0aee7873dc
chandran4725 <chandran.g.work@gmail.com>
2025-12-07 12:45:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.cpp
@@ -0,0 +1,51 @@
+class Solution {
+  public:
+    int convert(char c)
+    {
+        switch(c)
+        {
+            case 'I' :
+            return 1;
+        case 'V' :
+            return 5;
+        case 'X' :
+            return 10;
+        case 'L' :
+            return 50;
+        case 'C' :
+            return 100;
+        case 'D' :
+            return 500;
+        case 'M' :
+            return 1000;
+        default :
+            return 0;
+        }
+    }
+    int romanToDecimal(string &s) {
+        // code here
+        int sum = 0;
+        int n = s.size();
+        
+        for(int i=0;i<n;i++)
+        {
+           int curr = convert(s[i]);
+           
+           if(i+1 < n)
+           {
+               int next = convert(s[i+1]);
+               
+               if(curr < next)
+               {
+                   sum+=next-curr;
+                   i++;
+                   continue;
+               }
+           }
+           
+           sum+=curr;
+        }
+        
+        return sum;
+    }
+};
