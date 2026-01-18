8b57618a3e9797b316080fe914e2d6c9597a9873
chandran4725 <chandran.g.work@gmail.com>
2026-01-18 17:22:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,14 +1,53 @@
 class Solution {
   public:
+        int firstOccurence(vector<int>& arr, int x) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+            while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == x) {
+                ans = mid;
+                high = mid - 1;
+            } else if (arr[mid] > x)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return ans;
+    }
+
+    int secondOccurence(vector<int>& arr, int x) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+            while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == x) {
+                ans = mid;
+                low = mid + 1;
+            } else if (arr[mid] > x)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return ans;
+    }
+    
     int countFreq(vector<int>& arr, int target) {
         // code here
-        int occurrence = 0;
+        int first = firstOccurence(arr,target);
         
-        for(int i=0;i<arr.size();i++)
-        {
-            if(arr[i] == target) occurrence++;
-        }
+        if(first == -1) return 0;
         
-        return occurrence;
+        int second = secondOccurence(arr,target);
+        
+        return second - first + 1;
     }
 };
